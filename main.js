5861820cbf2cea3870716ebd1c39a7a132e4c96c
DaRealEndless <76487310+DaRealEndless@users.noreply.github.com>
2021-05-09 16:14:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    width: 350,
+    height: 350,
+    image_format: 'png',
+    png_quality: 100
+});
+camera = document.getElementById("Camera")
+
+Webcam.attach("#camera");
+
+function pic_take() {
+    Webcam.snap(function (data_url) {
+        document.getElementById("Result").innerHTML = '<img id="captured_img" src=' + data_url + '>';
+    });
+}
+
+console.log("ml5.version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("model_loaded");
+}
